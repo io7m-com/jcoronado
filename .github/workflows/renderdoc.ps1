23e7a8d0c44a9b253d267a21697f73ce12e58775
Mark Raynsford <code@io7m.com>
2026-03-17 12:54:31 +0000

    Update workflows.

--- .github/workflows/renderdoc.ps1
+++
@@ -1,4 +0,0 @@
-$uri = 'https://renderdoc.org/stable/1.41/RenderDoc_1.41_64.msi'
-Invoke-WebRequest -Uri $uri -OutFile renderdoc.msi -UseBasicParsing
-Start-Process msiexec.exe -Wait -ArgumentList '/i', (Resolve-Path renderdoc.msi), '/qn', '/norestart'
-& 'C:\Program Files\RenderDoc\qrenderdoc.exe' --version
